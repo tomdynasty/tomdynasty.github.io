81357186f4c2cb9f0b58098a6185def0f6ec153a
tom chen <asd88664422@gmail.com>
2020-03-07 14:53:06 +0800

    加入尋找目標與顯示地圖位置的圖示

--- src/Component/DrugStoreDom.jsx
+++ src/Component/DrugStoreDom.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { Card } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faLocationArrow } from '@fortawesome/free-solid-svg-icons';
 
 class DrugStoreDom extends Component {
   render() {
     return (
       <div>
-        <Card size="small" title="啄木鳥藥局">
+        <Card
+          size="small"
+          title="啄木鳥藥局"
+          extra={(
+            <div>
+              <FontAwesomeIcon icon={faEye} color="#73c0d8" size="lg" />
+              <span>&nbsp;&nbsp;&nbsp;</span>
+              <FontAwesomeIcon icon={faLocationArrow} color="#73c0d8" size="lg" />
+            </div>
+            )}
+        >
           <div className="drug-store-content">
             <i className="fas fa-camera fa-xs" />
             <div>- 台北市中正區汀州路3段125號</div>
@@ -25,7 +37,17 @@ class DrugStoreDom extends Component {
             </span>
           </div>
         </Card>
-        <Card size="small" title="吃吃吃藥局">
+        <Card
+          size="small"
+          title="吃吃吃藥局"
+          extra={(
+            <div>
+              <FontAwesomeIcon icon={faEye} color="#73c0d8" size="lg" />
+              <span>&nbsp;&nbsp;&nbsp;</span>
+              <FontAwesomeIcon icon={faLocationArrow} color="#73c0d8" size="lg" />
+            </div>
+            )}
+        >
           <div className="drug-store-content">
             <i className="fas fa-camera fa-xs" />
             <div>- 台北市中正區汀州路3段125號</div>
@@ -45,7 +67,17 @@ class DrugStoreDom extends Component {
             </span>
           </div>
         </Card>
-        <Card size="small" title="123藥局">
+        <Card
+          size="small"
+          title="123藥局"
+          extra={(
+            <div>
+              <FontAwesomeIcon icon={faEye} color="#73c0d8" size="lg" />
+              <span>&nbsp;&nbsp;&nbsp;</span>
+              <FontAwesomeIcon icon={faLocationArrow} color="#73c0d8" size="lg" />
+            </div>
+            )}
+        >
           <div className="drug-store-content">
             <i className="fas fa-camera fa-xs" />
             <div>- 台北市中正區汀州路3段125號</div>
