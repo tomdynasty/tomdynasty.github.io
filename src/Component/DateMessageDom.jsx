6351e06794a72517329fe08f1e8a68e50fea88b8
tom chen <asd88664422@gmail.com>
2020-08-10 02:57:21 +0800

    [add]current date

--- src/Component/DateMessageDom.jsx
+++ src/Component/DateMessageDom.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
+import PropTypes from 'prop-types';
 import faceMaskLogo from '../SVG/faceMask.svg';
 
 class DateMessageDom extends Component {
@@ -7,18 +8,18 @@ class DateMessageDom extends Component {
     return (
       <div>
         <div className="weekday">
-          星期六
+          {this.props.currentWeekday}
           <span>
             <img src={faceMaskLogo} alt="口罩圖片" width="50px" className="maskLogo" />
           </span>
         </div>
         <div className="date-info-group">
-          <div>2020-02-29</div>
+          <div>{this.props.currentFullDate}</div>
           <div>
             身分證末碼為
             <span className="id-card-last-code">
               <span> </span>
-              0,2,4,6,8
+              {this.props.idToPurchase}
               <span> </span>
             </span>
             可購買
@@ -28,5 +29,11 @@ class DateMessageDom extends Component {
     );
   }
 }
+
+DateMessageDom.propTypes = {
+  currentWeekday: PropTypes.string.isRequired,
+  currentFullDate: PropTypes.string.isRequired,
+  idToPurchase: PropTypes.string.isRequired,
+};
 
 export default DateMessageDom;
--- src/Container/DateMessage.jsx
+++ src/Container/DateMessage.jsx
@@ -3,8 +3,30 @@ import DateMessageDom from '../Component/DateMessageDom';
 
 class DateMessage extends Component {
   render() {
+    const today = new Date();
+    const weekday = new Array(7);
+    weekday[0] = '星期日';
+    weekday[1] = '星期一';
+    weekday[2] = '星期二';
+    weekday[3] = '星期三';
+    weekday[4] = '星期四';
+    weekday[5] = '星期`五';
+    weekday[6] = '星期六';
+
+    const currentWeekday = weekday[today.getDay()];
+    const currentFullDate = `${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`;
+
+    let idToPurchase = '1, 3, 5, 7, 9';
+    if ((today.getDay() === 0) || (today.getDay % 2 === 0)) {
+      idToPurchase = '0, 2, 4, 6, 8';
+    }
+
     return (
-      <DateMessageDom />
+      <DateMessageDom
+        currentWeekday={currentWeekday}
+        currentFullDate={currentFullDate}
+        idToPurchase={idToPurchase}
+      />
     );
   }
 }
